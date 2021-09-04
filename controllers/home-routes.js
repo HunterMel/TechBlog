e54fe59af03af30c00a56f358788a8493b44332a
Trayehunter1 <hunterjohnsoninfo@gmail.com>
2021-09-04 16:28:54 -0500

    update file errors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,46 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single post
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -36,14 +76,12 @@ router.get('/post/:id', (req, res) => {
         return;
       }
 
-      // serialize the data
       const post = dbPostData.get({ plain: true });
 
-      // pass data to template
       res.render('single-post', {
-         post,
-         loggedIn: req.session.loggedIn
-         });
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
@@ -59,23 +97,5 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
-
-
-router.get('/post/:id', (req, res) => {
-  const post = {
-    id: 1,
-    post_url: 'https://handlebarsjs.com/guide/',
-    title: 'Handlebars Docs',
-    created_at: new Date(),
-    vote_count: 10,
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
-  };
-
-  res.render('single-post', { post });
-});
-
 
 module.exports = router;
