9a13c22c9bd55d6cc7efe058ce4f071d2e268a1c
Trayehunter1 <hunterjohnsoninfo@gmail.com>
2021-09-04 16:19:26 -0500

    render logins

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,9 +3,11 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
-router.get('/', (req, res) => {
-  console.log('======================');
-  Post.findAll({
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
     attributes: [
       'id',
       'post_url',
@@ -29,9 +31,19 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
-      res.render('homepage', { posts });
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', {
+         post,
+         loggedIn: req.session.loggedIn
+         });
     })
     .catch(err => {
       console.log(err);
@@ -47,5 +59,23 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+
+router.get('/post/:id', (req, res) => {
+  const post = {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'Handlebars Docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  };
+
+  res.render('single-post', { post });
+});
+
 
 module.exports = router;
